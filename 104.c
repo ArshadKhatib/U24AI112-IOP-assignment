a26502053d5e711b1f49c400c4c739e20d493ab6
ArshadKhatib <u24ai112@aid.svnit.ac.in>
2024-11-26 19:29:29 +0530

    LAB 9&10

--- 104.c
+++ 104.c
@@ -1,38 +1,63 @@
-#include<stdio.h>
-int main()
-{
-    enum colour{white=1,red,green,blue,black};
-    int n;
-    printf("choose any number between (0-5)\nchoose '0' for exiting the program\n");
-   while(n!=0){
-    printf("Enter number:");
-   scanf("%d",&n);
-   switch (n)
-   { case 0: break;
-    case 1:{
-        printf("WHITE:#FFFFFF\n");
-        break;
+#include <stdio.h>
+
+// Enumeration of color names
+typedef enum {
+    RED,
+    GREEN, 
+    BLUE
+} Color;
+
+// Function to get hexadecimal color code based on enum
+const char* getColorHex(Color color) {
+    switch(color) {
+        case RED:     return "#FF0000";    // Bright Red
+        case GREEN:   return "#00FF00";    // Bright Green
+        case BLUE:    return "#0000FF";    // Bright Blue
+        default:      return "#000000";    // Black (default)
     }
-    case 2:{
-        printf("RED:#FF0000\n");
-        break;
-    }
-    case 3:{
-        printf("GREEN:#00FF00\n");
-        break;
-    }
-    case 4:{
-        printf("BLUE:#0000FF\n");
-        break;
 }
-    case 5:{
-        printf("BLACK:#000000\n");
-        break;
+
+// Function to print color name and its hex code
+void printColorInfo(Color color) {
+    const char* colorNames[] = {
+        "Red", "Green", "Blue"
+    };
+
+    printf("Color: %s\n", colorNames[color]);
+    printf("Hex Code: %s\n", getColorHex(color));
+}
+
+int main() {
+    // Demonstrate using the enum with switch statement
+    Color selectedColor = BLUE;
+
+    // Switch statement to handle color selection
+    switch(selectedColor) {
+        case RED:
+            printf("Selected a hot color!\n");
+            printColorInfo(RED);
+            break;
+        
+        case GREEN:
+            printf("Selected a nature color!\n");
+            printColorInfo(GREEN);
+            break;
+        
+        case BLUE:
+            printf("Selected a cool color!\n");
+            printColorInfo(BLUE);
+            break;
+        
+        default:
+            printf("Unknown color selected.\n");
     }
-    default:
-    {
-        printf("NO valid colour available\n");
+
+    // Additional examples
+    printf("\nAll Color Hex Codes:\n");
+    for (int i = 0; i < 10; i++) {
+        printColorInfo((Color)i);
+        printf("\n");
     }
-   }
-   }
+
+    return 0;
 }
